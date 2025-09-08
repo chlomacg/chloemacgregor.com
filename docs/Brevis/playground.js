bc2309183e76141b0a03c66812275db7a9ac8530
Chloe MacGregor <chloemacgregor29@gmail.com>
2025-09-08 14:04:49 -0500

    automated commit

--- docs/Brevis/playground.js
+++ docs/Brevis/playground.js
@@ -8,7 +8,7 @@ console.log("INITTING");
 	var count = textareas.length;
 	for(var i=0;i<count;i++){
     	textareas[i].onkeydown = function(e){
-        	if(e.shiftKey && (e.keyCode==9 || e.which==9)){
+        	if(e.shiftKey && (e.keyCode==9 || e.which==9)) {
             	e.preventDefault();
             	var s = this.selectionStart;
             	this.value = this.value.substring(0,this.selectionStart) + "	" + this.value.substring(this.selectionEnd);
